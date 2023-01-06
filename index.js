4418b87427b06925c227988b73b0bfa808482711
LincKim <lincolnkimutai@gmail.com>
2023-01-06 10:20:49 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code here
 const testEmployee = ['Gray', 'Worm', 'Security', 1]
-function createEmployeeRecord ([firstName, familyName, title, payPerHour], timeInEvents, timeOutEvents){
+let timeEvents = ['','']
+function createEmployeeRecord ([firstName, familyName, title, payPerHour], [timeInEvents, timeOutEvents]){
    
 }
 createEmployeeRecord(testEmployee)
